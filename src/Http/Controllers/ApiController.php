54fdbff9f116c3957743aaa94b1baa38b7742cb3
Simon Dann <carbontwelve@users.noreply.github.com>
2018-06-12 13:19:15 +0100

    Boilerplate controller methods, these will need breaking out

--- src/Http/Controllers/ApiController.php
+++ src/Http/Controllers/ApiController.php
@@ -8,13 +8,206 @@ use Zend\Diactoros\Response\JsonResponse;
 class ApiController extends Controller
 {
     /**
+     * GET: /
+     *
      * @param ServerRequestInterface $request
      * @param ResponseInterface $response
      * @param array $args
-     * @return \Zend\Diactoros\Response\JsonResponse
+     * @return JsonResponse
      */
-    public function index(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    public function getFetch(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
     {
-        return new JsonResponse(['msg' => 'hello world']);
+        return new JsonResponse(['msg' => 'fetch']);
+    }
+
+    /**
+     * POST: /new
+     *
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return JsonResponse
+     */
+    public function postNew(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    {
+        return new JsonResponse(['msg' => 'fetch']);
+    }
+
+    /**
+     * GET: /count
+     *
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return JsonResponse
+     */
+    public function getCount(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    {
+        return new JsonResponse(['msg' => 'fetch']);
+    }
+
+    /**
+     * POST: /count
+     *
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return JsonResponse
+     */
+    public function postCounts(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    {
+        return new JsonResponse(['msg' => 'fetch']);
+    }
+
+    /**
+     * GET: /feed
+     *
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return JsonResponse
+     */
+    public function getFeed(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    {
+        return new JsonResponse(['msg' => 'fetch']);
+    }
+
+    /**
+     * GET: /id/<int:id>
+     *
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return JsonResponse
+     */
+    public function getView(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    {
+        return new JsonResponse(['msg' => 'fetch']);
+    }
+
+    /**
+     * PUT: /id/<int:id>
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return JsonResponse
+     */
+    public function putEdit(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    {
+        return new JsonResponse(['msg' => 'fetch']);
+    }
+
+    /**
+     * DELETE: /id/<int:id>
+     *
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return JsonResponse
+     */
+    public function deleteDelete(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    {
+        return new JsonResponse(['msg' => 'fetch']);
+    }
+
+    /**
+     * GET: /id/<int:id>/<any(edit,activate,delete):action>/<string:key>
+     *
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return JsonResponse
+     */
+    public function getModerate(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    {
+        return new JsonResponse(['msg' => 'fetch']);
+    }
+
+    /**
+     * POST: /id/<int:id>/<any(edit,activate,delete):action>/<string:key>
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return JsonResponse
+     */
+    public function postModerate(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    {
+        return new JsonResponse(['msg' => 'fetch']);
+    }
+
+    /**
+     * POST: /id/<int:id>/like
+     *
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return JsonResponse
+     */
+    public function postLike(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    {
+        return new JsonResponse(['msg' => 'fetch']);
+    }
+
+    /**
+     * POST: /id/<int:id>/dislike
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return JsonResponse
+     */
+    public function postDislike(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    {
+        return new JsonResponse(['msg' => 'fetch']);
+    }
+
+    /**
+     * GET: /demo
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return JsonResponse
+     */
+    public function getDemo(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    {
+        return new JsonResponse(['msg' => 'fetch']);
+    }
+
+    /**
+     * POST: /preview
+     *
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return JsonResponse
+     */
+    public function postPreview(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    {
+        return new JsonResponse(['msg' => 'fetch']);
+    }
+
+    /**
+     * POST: /login
+     *
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return JsonResponse
+     */
+    public function postLogin(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    {
+        return new JsonResponse(['msg' => 'fetch']);
+    }
+
+    /**
+     * GET: /admin
+     *
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return JsonResponse
+     */
+    public function getAdmin(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    {
+        return new JsonResponse(['msg' => 'fetch']);
     }
 }
