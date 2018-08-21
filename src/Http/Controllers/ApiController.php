5f4365fed6f21c09a958ac3b37d4273ad472c382
Simon Dann <carbontwelve@users.noreply.github.com>
2018-08-21 23:36:07 +0100

    :construction: Porting some parts of comments.py

--- src/Http/Controllers/ApiController.php
+++ src/Http/Controllers/ApiController.php
@@ -1,17 +1,138 @@
 <?php
 namespace App\Http\Controllers;
 
+use Adbar\Dot;
 use App\Entities\Comment;
+use App\Entities\Thread;
 use App\Repositories\Comments;
+use App\Repositories\Threads;
+use Doctrine\ORM\EntityManagerInterface;
+use Photogabble\Tuppence\App;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\JsonResponse;
 
 class ApiController extends Controller
 {
+
+    /**
+     * Comment fields, that can be submitted in request.
+     * @var array
+     */
+    private $accept = ['text', 'author', 'website', 'email', 'parent', 'title', 'notification'];
+
+    /**
+     * Default fields that are sent in response. (Public fields.)
+     * @var array
+     */
+    private $fields = ['id', 'parent', 'text', 'author', 'website', 'mode', 'created', 'modified', 'likes', 'dislikes', 'hash', 'gravatar_image', 'notification'];
+
+    /**
+     * @var array
+     */
+    private $configuration;
+
+    /**
+     * @var bool
+     */
+    private $moderation;
+
+    public function __construct(EntityManagerInterface $entityManager, App $app)
+    {
+        parent::__construct($entityManager, $app);
+
+        /** @var Dot $config */
+        $config = $app->getContainer()->get('config');
+        $this->moderation = $config->get('moderation.enabled', true);
+        $this->configuration = new Dot($config->get('general', []));
+    }
+
+    /**
+     * POST: /new
+     *
+     * Port of isso python isso.views.comments.fetch
+     * @see https://github.com/posativ/isso/blob/master/isso/views/comments.py#L247
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return JsonResponse
+     */
+    public function postNew(ServerRequestInterface $request, ResponseInterface $response, array $args = []): ResponseInterface
+    {
+        $q = new Dot(array_filter($request->getQueryParams(), function($k) {
+            return in_array($k, $this->accept);
+        }, ARRAY_FILTER_USE_KEY));
+
+        foreach (["author", "email", "website", "parent"] as $k) {
+            if (!$q->has($k)){
+                $q->set($k, '');
+            }
+        }
+
+        $v = new \App\Http\Validation\Comment();
+        if (! $v->verify($q->flatten())) {
+            return new JsonResponse($v->getErrors(), 400);
+        }
+
+        foreach (["author", "email", "website"] as $field) {
+            $q->set($field, htmlspecialchars($q->get($field, '')));
+        }
+
+        if (strlen($q->get('website')) > 0) {
+            $q->set('website', $this->normaliseUrl($q->get('website')));
+        }
+
+        $q->set('mode', ( $this->moderation ? 2 : 1 ));
+
+        // @todo upgrade Tuppence to PSR-15 and use https://github.com/middlewares/psr15-middlewares to get ip
+        $q->set('remote_addr', '127.0.0.1');
+
+        /** @var Threads $threads */
+        $threads = $this->entityManager->getRepository(Thread::class);
+
+
+        if ($threads->contains($q->get('uri'))) {
+            $thread = $threads->getThreadByUri($q->get('uri'));
+        } else {
+            // If title not set then attempt to parse the title of the referring url
+            if (! $q->has('title')) {
+                // @todo https://github.com/posativ/isso/blob/master/isso/views/comments.py#L271-L285
+            }
+
+            try{
+                $thread = $threads->new($q->get('uri'), $q->get('title'));
+            } catch (\Exception $e) {
+                return new JsonResponse(['error' => 'Database error'],  500);
+            }
+
+        }
+
+        // @todo finish me
+
+        $this->entityManager->flush(); // Commit all changes to disk...
+
+        return new JsonResponse([]);
+    }
+
+    /**
+     * Port of isso python isso.views.comments.normalize
+     * @see https://github.com/posativ/isso/blob/master/isso/views/comments.py#L61
+     * @param string $url
+     * @return string
+     */
+    private function normaliseUrl(string $url): string
+    {
+        if (strpos($url, 'http://') === false || strpos($url, 'https://') === false) {
+            return 'http://'.$url;
+        }
+        return $url;
+    }
+
     /**
      * GET: /
      *
+     * Port of isso python isso.views.comments.fetch
+     * @see https://github.com/posativ/isso/blob/master/isso/views/comments.py#L247
      * @param ServerRequestInterface $request
      * @param ResponseInterface $response
      * @param array $args
@@ -45,28 +166,6 @@ class ApiController extends Controller
             }, $replies),
             'total_replies' => $count
         ]);
-    }
-
-    /**
-     * POST: /new
-     *
-     * @param ServerRequestInterface $request
-     * @param ResponseInterface $response
-     * @param array $args
-     * @return JsonResponse
-     */
-    public function postNew(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
-    {
-        $q = $request->getQueryParams();
-
-        $args = array_merge([
-            'uri' => isset($q['uri']) ? (string) $q['uri'] : '',
-            'text' => isset($q['text']) ? (string) $q['text'] : '',
-        ], $args);
-
-        $id = null; // @todo finish this method
-
-        return new JsonResponse(['id' => $id]);
     }
 
     /**
--- src/Repositories/Threads.php
+++ src/Repositories/Threads.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories;
 
+use App\Entities\Thread;
 use Doctrine\ORM\EntityRepository;
 
 class Threads extends EntityRepository
@@ -9,11 +10,38 @@ class Threads extends EntityRepository
     /**
      * Undocumented.
      *
+     * Port of isso python isso.db.threads.__contains__
+     * @see https://github.com/posativ/isso/blob/master/isso/db/threads.py#L21
+     * @param string $uri
+     * @return bool
+     */
+    public function contains(string $uri): bool
+    {
+        return false; // @todo
+    }
+
+    /**
+     * Undocumented.
+     *
+     * Port of isso python isso.db.threads.__getitem__
+     * @see https://github.com/posativ/isso/blob/master/isso/db/threads.py#L25
+     * @param string $uri
+     * @return Thread
+     */
+    public function getThreadByUri(string $uri): Thread
+    {
+        // @todo
+    }
+
+    /**
+     * Undocumented.
+     *
      * Port of isso python isso.db.threads.get
      * @see https://github.com/posativ/isso/blob/master/isso/db/threads.py#L28
      * @param int $id
+     * @return Thread
      */
-    public function get(int $id)
+    public function get(int $id): Thread
     {
         // @todo
     }
@@ -23,11 +51,18 @@ class Threads extends EntityRepository
      *
      * Port of isso python isso.db.threads.new
      * @see https://github.com/posativ/isso/blob/master/isso/db/threads.py#L31
+     * @throws \Doctrine\ORM\ORMException
      * @param string $uri
      * @param string $title
+     * @return Thread
      */
-    public function new(string $uri, string $title)
+    public function new(string $uri, string $title): Thread
     {
-        // @todo
+        $thread = new Thread();
+        $thread->setUri($uri);
+        $thread->setTitle($title);
+
+        $this->getEntityManager()->persist($thread);
+        return $thread;
     }
 }
