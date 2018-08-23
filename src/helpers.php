5f118a71b5b29755bbeb0f0edc92a21d0a2b37b5
Simon Dann <carbontwelve@users.noreply.github.com>
2018-08-23 23:33:50 +0100

    :sparkles: added origin helper

--- src/helpers.php
+++ src/helpers.php
@@ -1,8 +1,16 @@
 <?php
 
-if (! function_exists('env')) {
-    function env($key, $default = null) {
-        if ($e = getenv($key)){
+if (!function_exists('env')) {
+    /**
+     * Helper for getting environment variables or default on fail.
+     *
+     * @param string $key
+     * @param null|mixed $default
+     * @return array|false|null|string
+     */
+    function env(string $key, $default = null)
+    {
+        if ($e = getenv($key)) {
             return $e;
         }
 
@@ -10,3 +18,29 @@ if (! function_exists('env')) {
     }
 }
 
+if (!function_exists('origin')) {
+    /**
+     * Return a function that returns a valid HTTP Origin or localhost
+     * if none found.
+     * @see https://github.com/posativ/isso/blob/5bc176d85b64eac331f578d0657ac26be40b2470/isso/wsgi.py#L78
+     * @param string|null $url
+     * @return string
+     */
+    function origin(string $url = null): string
+    {
+        if ($origin = env('ISSO_CORS_ORIGIN')) {
+            return $origin;
+        }
+
+        if (is_null($url) || empty($url)) {
+            return "http://invalid.local";
+        }
+
+        if ($origin = env('HTTP_ORIGIN', env('HTTP_REFERER'))) {
+            return $origin;
+        }
+
+        return $url;
+    }
+
+}
